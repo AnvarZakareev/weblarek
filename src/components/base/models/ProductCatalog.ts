c412b6c5f3f6805d60ad2c8570344f5da6197f9f
AnvarZakareev <zanvarr@mail.ru>
2025-10-09 23:28:37 +0800

    add ProductCataloge.ts

---
+++ src/components/base/models/ProductCatalog.ts
@@ -0,0 +1,34 @@
+interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+export class ProductCatalog {
+    ProductArray: IProduct[];
+    ProductSelected: IProduct;
+    constructor(ProductArray: IProduct[], ProductSelected: IProduct)
+    {
+        this.ProductArray = ProductArray;
+        this.ProductSelected = ProductSelected
+    }
+    saveArray(ProductArray: IProduct[]) {
+        this.ProductArray = ProductArray;
+    }
+    getArray(): IProduct[] {
+        return this.ProductArray;
+    }
+    getProductById(id: string): IProduct | undefined {
+        return this.ProductArray.find(IProduct => IProduct.id == id);
+    }
+    saveProductSelected(IProduct: IProduct): IProduct {
+        this.ProductSelected = IProduct;
+        return this.ProductSelected
+    }
+    getProductSelected(): IProduct {
+        return this.ProductSelected
+    }
+}
