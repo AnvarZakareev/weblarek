9129e78841b45a06f20b3d049312f84ef77e371e
AnvarZakareev <zanvarr@mail.ru>
2025-10-10 00:48:05 +0800

    edit basket.ts

--- src/components/base/models/Basket.ts
+++ src/components/base/models/Basket.ts
@@ -8,23 +8,17 @@ interface IProduct {
 }
 
 export class Basket {
-    // хранит массив товаров, выбранных покупателем для покупки
     ProductArrayInBasket: IProduct[];
-    // ProductAdded: IProduct;
-    // В конструктор передается массив ВЫБРАННЫХ всех товаров
     constructor(ProductArrayInBasket: IProduct[]) 
     {
         this.ProductArrayInBasket = ProductArrayInBasket;
     }
-    // получение массива товаров, которые находятся в корзине
     getProductArrayInBasket(): IProduct[] {
         return this.ProductArrayInBasket;
     }
-    // добавление товара, который был получен в параметре, в массив корзины
     addProductInBasket(Product: IProduct) {
         this.ProductArrayInBasket.push(Product);
     }
-    // удаление товара, полученного в параметре из массива корзины
     delProductInBasket(DelProduct: IProduct) {
         const index = this.ProductArrayInBasket.findIndex(product => product.id === DelProduct.id);
         if (index !== -1) {
@@ -33,11 +27,9 @@ export class Basket {
         }
         return false;
     }
-    // очистка корзины
     clearProductInBasket() {
         this.ProductArrayInBasket = [];
     }
-    // получение стоимости всех товаров в корзине
     summProductInBasket(): number {
         let sum = 0
         this.ProductArrayInBasket.forEach(element => {
@@ -47,11 +39,9 @@ export class Basket {
         );
         return sum;
     }
-    // получение количества товаров в корзине
     getLengthProductInBasket(): number {
         return this.ProductArrayInBasket.length;
     }
-    // проверка наличия товара в корзине по его id, полученного в параметр метода
     checkProductInBasketById(id: string): boolean {
         return this.ProductArrayInBasket.some(product => product.id === id);
     }
