2cc56998534ba4e20401d5374cce06937c82e94e
AnvarZakareev <zanvarr@mail.ru>
2025-10-14 01:20:25 +0800

    rev 2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Buyer } from './components/models/Buyer';
 import { TPayment } from './types/index'
 import { CompositionAPI } from './components/base/CompositionApi';
 import { Api } from './components/base/Api';
-import { API_URL } from './utils/constants'
+import { API_URL, API_URL_WORKS } from './utils/constants'
 
 //#region test ProductCatalog
 
@@ -15,8 +15,6 @@ const productsModel = new ProductCatalog();
 productsModel.setItems(apiProducts.items);
 console.log(`Массив товаров из каталога: `, productsModel.getItems());
 let test1 = productsModel.getProductById("c101ab44-ed99-4a54-990d-47aa2bb4e7d9");
-// let test2 = productsModel.getProductById("b06cde61-912f-4663-9751-09956c0eed67");
-// let test3 = productsModel.getProductById("412bcf81-7e75-4e70-bdb9-d3c73c9803b7");
 console.log(`получение одного товара по его id`, productsModel.getProductById("854cef69-976d-4c2a-a18c-2aa45046c390"))
 if (test1) {
   console.log(`сохранение товара для подробного отображения`, productsModel.saveProductSelected(test1))
@@ -77,22 +75,17 @@ console.log(`валидация данных`, bayerIncorrectModal.validBuyerDat
 //#region test CompositionAPI
 
 async function main() {
-  const apiInstance = new Api(API_URL);
+  const apiInstance = new Api(API_URL_WORKS);
+  console.log(apiInstance)
   const catalog = new CompositionAPI(apiInstance);
   
   try {
-    const productsModel2 = new ProductCatalog();
-    const basketModel2 = new Basket(productsModel2.getItems())
     // Получение каталога товаров
     const productList = await catalog.fetchProducts();
     // Сохранение товаров
-    productsModel2.setItems(productList.items)
-
-    console.log(`Наконец то получили массив товаров от сервера`, productsModel2)
-    basketModel2.clearBasket()
-    basketModel2.addProductInBasket(apiProducts.items[3]);
-    console.log(`обновленная корзина`, basketModel2)
-
+    productsModel.setItems(productList.items)
+    console.log(`Наконец то получили массив товаров от сервера`, productsModel)
+    
   } catch (error) {
     console.error('Ошибка при загрузке каталога:', error);
   }
