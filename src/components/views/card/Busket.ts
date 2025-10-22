c7cbab213ada7e7946b381ad65cb6055b0337dbe
AnvarZakareev <zanvarr@mail.ru>
2025-10-23 00:02:37 +0800

    edit basket.ts

--- src/components/views/card/Busket.ts
+++ src/components/views/card/Busket.ts
@@ -1,21 +1,28 @@
 import { ensureElement } from "../../../utils/utils";
 import { Component } from "../../base/Component";
+import { IEvents } from "../../base/Events";
 
 
 interface IBasket {
-    cards: Card[];
-    allPrice: number;
+    cards: null;
+    sum: number;
 }
 
 export class Basket extends Component<IBasket> {
+    protected basketPrice: HTMLElement;
     protected designButtom: HTMLButtonElement;
+    protected list: HTMLElement;
 
-    constructor(protected events: IEvents, container: HTMLElement) {
+    constructor(protected events: IEvents, container: HTMLElement, list: HTMLElement) {
         super(container);
 
-        this.elmt = ensureElement<HTMLElement>(
-            '.basket__list'
-            this.elmt
+        this.basketPrice = ensureElement<HTMLElement>(
+            '.basket__price',
+            this.container
+        );
+        this.list = ensureElement<HTMLElement>(
+            '.basket__list',
+            this.container
         );
         this.designButtom = ensureElement<HTMLButtonElement>(
             '.basket__button',
@@ -30,7 +37,9 @@ export class Basket extends Component<IBasket> {
 
     }
 
-    set cards(item: Card[])
+    set cards(item: null) {
+        
+    }
 }
 
 
