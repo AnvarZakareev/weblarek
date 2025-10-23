53b2ceb27c3e597c9a1ea52825375f852b899aa2
AnvarZakareev <zanvarr@mail.ru>
2025-10-23 17:06:27 +0800

    add Order.ts

--- src/components/views/card/CardInBusket.ts
+++ src/components/views/card/CardInBusket.ts
@@ -7,7 +7,7 @@ export class CardInBusket extends Card<IProduct> {
     protected itemElement: HTMLElement;
     protected deleteElementButton: HTMLButtonElement;
 
-    constructor(protected events: IEvents, container: HTMLElement,) {
+    constructor(protected events: IEvents, container: HTMLElement) {
         super(container);
 
         this.itemElement = ensureElement<HTMLElement>('.basket__item-index', this.container);
---
+++ src/components/views/form/Order.ts
@@ -0,0 +1,36 @@
+import { IBuyer, TPayment } from "../../../types"
+import { ensureElement } from "../../../utils/utils";
+import { IEvents } from "../../base/Events";
+import { Form } from "./Form"
+
+
+export type TOrder = Pick<IBuyer, 'payment' | 'address'>
+
+export class Order extends Form<TOrder> {
+    protected buttonPayment: HTMLButtonElement;
+    protected adresBuyer: HTMLInputElement;
+    protected buttonNext: HTMLButtonElement;
+
+    constructor(protected events: IEvents, container: HTMLElement){
+        super(container);
+
+        this.buttonPayment = ensureElement<HTMLButtonElement>('.button_alt', this.container);
+        this.adresBuyer = ensureElement<HTMLInputElement>('.form__input', this.container);
+        this.buttonNext = ensureElement<HTMLButtonElement>('.basket__item-index', this.container);
+        
+        this.buttonNext.addEventListener('click', () => {
+            this.events.emit('adres:send')
+        })
+    }
+
+    // set payment(value: TPayment) {
+    //     this.payment.
+    // }
+
+
+    set orderAdress(value: string) {
+        this.adresBuyer.textContent = value;
+    }
+
+
+}
