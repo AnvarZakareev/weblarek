ca2347daf5fd1f06d97c3da0c7275296e0a0662e
AnvarZakareev <zanvarr@mail.ru>
2025-10-25 01:12:18 +0800

    edit gallery

--- src/components/views/Gallary.ts
+++ src/components/views/Gallary.ts
@@ -6,18 +6,15 @@ interface IGallary {
 }
 
 export class Gallery<T> extends Component<IGallary> {
-    protected catalogElement: HTMLElement;
+    // protected catalogElement: HTMLElement;
 
     constructor(container: HTMLElement) {
         super(container);
 
-        this.catalogElement = ensureElement<HTMLElement>('.gallery', this.container);
+        // this.catalogElement = ensureElement<HTMLElement>('.gallery', this.container);
     }
 
-    set catalog(value: HTMLElement[]) {
-        this.catalogElement.innerHTML = '';
-        for (const element of value) {
-            this.catalogElement.appendChild(element);
-        }
+    protected set catalog(value: HTMLElement[]) {
+        this.container.replaceChildren(...value);
     }
 }
